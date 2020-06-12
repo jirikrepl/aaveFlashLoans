ce5ec4863effc752ba791bfc87e44bf2fdee466e
ChrisSorrell <52987387+ChrisSorrell@users.noreply.github.com>
2020-06-11 21:57:37 -0400

    Update FlashLoanReceiverBase.sol

--- contracts/flashloan/base/FlashLoanReceiverBase.sol
+++ contracts/flashloan/base/FlashLoanReceiverBase.sol
@@ -1,51 +1,44 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.6;
 
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
-import "../interfaces/IFlashLoanReceiver.sol";
-import "../../interfaces/ILendingPoolAddressesProvider.sol";
-import "../../interfaces/INetworkMetadataProvider.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "./IFlashLoanReceiver.sol";
+import "./ILendingPoolAddressesProvider.sol";
+import "../utils/Withdrawable.sol";
 
-contract FlashLoanReceiverBase is IFlashLoanReceiver {
+abstract contract FlashLoanReceiverBase is IFlashLoanReceiver, Withdrawable {
 
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
-    ILendingPoolAddressesProvider addressesProvider;
+    address constant ethAddress = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    ILendingPoolAddressesProvider public addressesProvider;
 
-    constructor(ILendingPoolAddressesProvider _provider) public {
-        addressesProvider = _provider;
+    constructor(address _addressProvider) public {
+        addressesProvider = ILendingPoolAddressesProvider(_addressProvider);
     }
 
-    function () external payable {
-    }
+    receive() payable external {}
 
-    function transferFundsBackToPoolInternal(address _reserve, uint _amount) internal {
-
+    function transferFundsBackToPoolInternal(address _reserve, uint256 _amount) internal {
         address payable core = addressesProvider.getLendingPoolCore();
-
-        transferInternal(core,_reserve, _amount);
+        transferInternal(core, _reserve, _amount);
     }
 
-    function transferInternal(address payable _destination, address _reserve, uint  _amount) internal {
-        if(_reserve == INetworkMetadataProvider(addressesProvider.getNetworkMetadataProvider()).getEthereumAddress()) {
-            _destination.transfer(_amount);
+    function transferInternal(address payable _destination, address _reserve, uint256 _amount) internal {
+        if(_reserve == ethAddress) {
+            (bool success, ) = _destination.call{value: _amount}("");
+            require(success == true, "Couldn't transfer ETH");
             return;
         }
-
         IERC20(_reserve).safeTransfer(_destination, _amount);
-
-
     }
 
     function getBalanceInternal(address _target, address _reserve) internal view returns(uint256) {
-        if(_reserve == INetworkMetadataProvider(addressesProvider.getNetworkMetadataProvider()).getEthereumAddress()) {
-
+        if(_reserve == ethAddress) {
             return _target.balance;
         }
-
         return IERC20(_reserve).balanceOf(_target);
-
     }
 }
